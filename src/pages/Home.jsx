4040247c52bb37127b9ecb868324ac07b772e2d4
Deshan Vimukthi <vimudeshan123@gmail.com>
2023-02-18 01:27:42 +0530

    fixed error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
   return (
     <div className="home">
       <div className="posts">
-        {posts && posts.map((post) =>(
+        {posts.map((post)  =>(
           <div className="post" key={post.id}>
             <div className="img">
               <img src={`../upload/${post.img}`} alt="" />
