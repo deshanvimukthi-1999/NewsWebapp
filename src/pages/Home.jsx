ee683d438bf6234320c1d1d374e5b9c9c27abe69
Deshan Vimukthi <vimudeshan123@gmail.com>
2023-02-18 02:27:51 +0530

    fixed error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,14 +13,14 @@ const Home = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(`/posts`);
+        const res = await axios.get(`/posts${cat}`);
         setPosts(res.data);
       } catch (err) {
         console.log(err);
       }
     };
     fetchData();
-  }, );
+  }, [cat]);
 
   const getText = (html) =>{
     const doc = new DOMParser().parseFromString(html, "text/html")
